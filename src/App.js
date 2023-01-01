c547c2e217a55107447636db5c944372cde3caf2
Yogesh <yogeshkhadka34@gmail.com>
2023-01-01 18:40:48 +0545

    class components and functional components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World in React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          By Yogesh
-        </a>
-      </header>
+      <Greet></Greet>
+      <Welcome></Welcome>
     </div>
   );
 }
